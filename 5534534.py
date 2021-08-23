fa50f5968177dfbbd55def5e175597fa52d41a68
lxstorexxd <75565271+lxstorexxd@users.noreply.github.com>
2021-08-23 21:51:28 +0300

    Add files via upload

---
+++ 5534534.py
@@ -0,0 +1,23 @@
+import fake_useragent
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+
+url = f'https://www.kinopoisk.ru/chance/?item=true&count=1&max_years=2021&min_years=2012'
+
+
+user = fake_useragent.UserAgent().random
+header = {
+    'user-agent': user
+}
+
+session = requests.Session()
+response = session.post(url, headers=header).json()
+
+for req in response:
+    soup = BeautifulSoup(req, 'lxml')
+
+    print(response)
+    print(soup)
